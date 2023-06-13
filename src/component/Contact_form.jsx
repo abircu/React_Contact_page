e08e6721fe8539b2b261b7d2157ba6e8ca85be40
abircu <mdabircse4873@gmail.com>
2023-06-14 01:42:27 +0600

    incomplete task

---
+++ src/component/Contact_form.jsx
@@ -0,0 +1,13 @@
+
+import Button from "./Button.jsx";
+import styles from"./contact_form.module.css";
+const Contact_form = () => {
+  return <section className={styles.container}>
+    <div className={styles. contact_form}>
+       <Button/>
+    </div>
+    <div className={styles.contact_image}></div>
+  </section>;
+};
+
+export default Contact_form;
